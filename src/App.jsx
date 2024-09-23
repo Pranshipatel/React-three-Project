eb0fa26a6be381efd0664f740b31b86b733c0769
Pranshi Patel <patelpranshi745@gmail.com>
2024-09-23 22:08:50 +0530

    basic

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import "./style.css"
+import { OrbitControls } from '@react-three/drei'
+
+const App = () => {
+  return (
+   <Canvas>
+    <OrbitControls />
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial />
+    </mesh>
+   </Canvas>
+  )
+}
+
+export default App
